a846aae4508d13dc27e57f1a12caca029af8e5f9
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-12 10:45:56 -0600

    Simplify the github example

--- github.lua
+++ github.lua
@@ -2,7 +2,8 @@ local jsonapi = require "jsonapi"
 
 local github = jsonapi.createProxy("https://api.github.com")
 
-local reply = github:get "/repos/vance-group/vr-jugglua/issues"
+--local reply = github:get "/repos/vance-group/vr-jugglua/issues"
+local reply = github:get()
 
 for k, v in pairs(reply) do
 	print(k,v)
