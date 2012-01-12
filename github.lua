6bdd9af1ae1595151b8134ba9f4c16040cca5af8
Ryan Pavlik <rpavlik@iastate.edu>
2012-01-12 12:10:36 -0600

    Pretty up github.lua

--- github.lua
+++ github.lua
@@ -1,10 +1,14 @@
+
+--An example of usage
 local jsonapi = require "jsonapi"
+require "std"
+
 
 local github = jsonapi.createProxy("https://api.github.com")
 
---local reply = github:get "/repos/vance-group/vr-jugglua/issues"
-local reply = github:get()
+local issues, code, response = github:get "/repos/vance-group/vr-jugglua/issues"
 
+prettytostring(issues)
 for k, v in pairs(reply) do
 	print(k,v)
 end
